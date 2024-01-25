f14d7a752ab03f98be7a979775793b59c0af92bd
kannstas <k.annstas@gmail.com>
2024-01-25 15:28:16 +0300

    update second task (fast)

--- src/RabbitBooks.java
+++ src/RabbitBooks.java
@@ -1,36 +1,27 @@
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class RabbitBooks {
     public static void main(String[] args) {
-        Map<Integer, Integer> heightOfBookToNumberOfSuchBooks = new HashMap<>();
 
         Scanner in = new Scanner(System.in);
         int numberOfBooks = Integer.parseInt(in.nextLine());
 
-        String[] booksHeights = in.nextLine().split(" ");
-
-
-        for (int i = 0; i < numberOfBooks; i++) {
-            int counter = 1;
-            for (int j = i + 1; j < numberOfBooks; j++) {
-                if (booksHeights[i].equals(booksHeights[j])) {
-                    counter++;
-                }
-            }
-            if (!(heightOfBookToNumberOfSuchBooks.containsKey(Integer.valueOf(booksHeights[i])))) {
-                heightOfBookToNumberOfSuchBooks.put(
-                        Integer.valueOf(booksHeights[i]),
-                        counter
+        Map<Integer, Integer> result = Arrays.stream(in.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(
+                        Collectors.toMap(
+                                (height) -> height,
+                                (height) -> 1,
+                                (oldNumber, newNumber) -> oldNumber + newNumber
+                        )
                 );
-            }
 
-        }
+        int minNumbersOfStacks = result.keySet().size();
 
-        int minNumbersOfStacks = heightOfBookToNumberOfSuchBooks.keySet().size();
-
-        String sortedHeightsOfBooks = heightOfBookToNumberOfSuchBooks.values().stream()
+        String sortedHeightsOfBooks = result.values().stream()
                 .sorted()
                 .map(String::valueOf)
                 .collect(Collectors.joining(" "));
@@ -38,7 +29,5 @@ public class RabbitBooks {
 
         System.out.println(minNumbersOfStacks);
         System.out.println(sortedHeightsOfBooks);
-
-
     }
 }
